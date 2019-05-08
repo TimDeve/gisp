4cd11817c420d72ef7fd89164bcbebc5d329dcfe
Tim Dévé <tdeve@thoughtworks.com>
2019-05-08 15:08:26 +0100

    Adds eval for lists

--- eval/eval.go
+++ eval/eval.go
@@ -1,8 +1,11 @@
 package eval
 
 import (
+	"errors"
+
 	"github.com/TimDeve/gisp/lexer"
 	"github.com/TimDeve/gisp/parser"
+	"github.com/TimDeve/gisp/stdlib"
 	"github.com/TimDeve/gisp/value"
 )
 
@@ -14,5 +17,37 @@ func Eval(input string) (value.Value, error) {
 		return value.Number{0.0}, err
 	}
 
-	return values[0], err
+	var newValues []value.Value
+
+	for _, val := range values {
+		if val.GetType() == value.SEXP {
+			newVal, err := EvalExpression(val.(value.Sexp))
+			if err != nil {
+				return value.Number{0.0}, err
+			}
+			newValues = append(newValues, newVal)
+		} else {
+			newValues = append(newValues, val)
+		}
+	}
+
+	return newValues[len(values)-1], err
+}
+
+func EvalExpression(expr value.Sexp) (value.Value, error) {
+	values := expr.GetValue()
+	first := values[0]
+	rest := values[1:len(values)]
+
+	if first.GetType() == value.SYMBOL {
+		v := first.(value.Symbol)
+		f, ok := stdlib.GetLib(v.GetValue())
+		if !ok {
+			return value.Number{}, errors.New("Symbol not found")
+		}
+		res, err := f(rest)
+		return res, err
+	} else {
+		return expr, nil
+	}
 }
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -1,12 +1,13 @@
 package eval
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/TimDeve/gisp/value"
 )
 
-func TestParseInt(t *testing.T) {
+func TestEvalInt(t *testing.T) {
 	result, err := Eval("1")
 
 	if err != nil {
@@ -25,7 +26,7 @@ func TestParseInt(t *testing.T) {
 	}
 }
 
-func TestParseFloat(t *testing.T) {
+func TestEvalFloat(t *testing.T) {
 	result, err := Eval("1.35")
 
 	if err != nil {
@@ -40,5 +41,61 @@ func TestParseFloat(t *testing.T) {
 	number := result.(value.Number)
 	if number.GetValue() != 1.35 {
 		t.Errorf(`Eval("1.35") = %f; want 1.35`, result)
+	}
+}
+
+func TestEvalList(t *testing.T) {
+	result, err := Eval("(1 1.0 2.4)")
+
+	expected := value.Sexp{
+		[]value.Value{
+			value.Number{1.0},
+			value.Number{1.0},
+			value.Number{2.4},
+		},
+	}
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
+
+func TestEvalAdd(t *testing.T) {
+	result, err := Eval("(+ 1 1)")
+
+	if err != nil {
+		t.Errorf(`Eval("(+ 1 1)") returned an error: %s`, err.Error())
+	}
+
+	if result.GetType() != value.NUMBER {
+		t.Errorf(`Did not return a number`)
+		return
+	}
+
+	number := result.(value.Number)
+	if number.GetValue() != 2.0 {
+		t.Errorf(`Eval("(+ 1 1)") = %f; want 2.0`, result)
+	}
+}
+
+func TestEvalReturnsLastValue(t *testing.T) {
+	result, err := Eval("(+ 1 1) (+ 1 2)")
+
+	if err != nil {
+		t.Errorf(`Eval("(+ 1 1) (+ 1 2)") returned an error: %s`, err.Error())
+	}
+
+	if result.GetType() != value.NUMBER {
+		t.Errorf(`Did not return a number`)
+		return
+	}
+
+	number := result.(value.Number)
+	if number.GetValue() != 3.0 {
+		t.Errorf(`Eval("(+ 1 1) (+ 1 2)") = %f; want 2.0`, result)
 	}
 }
