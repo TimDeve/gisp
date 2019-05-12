cd38b6bd4b46f88ce15c99787f2041d9fdf88bc1
Tim Dévé <tdeve@thoughtworks.com>
2019-05-12 20:39:15 +0200

    Adds greater than (>) to stdlib

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -54,10 +54,35 @@ func equal(values []value.Value) (value.Value, error) {
 	return result, nil
 }
 
+func greaterThan(values []value.Value) (value.Value, error) {
+	if len(values) == 0 {
+		return value.NewBoolean(false), nil
+	} else if len(values) == 1 {
+		return value.NewBoolean(true), nil
+	}
+
+	for i := 0; i < len(values); i++ {
+		if !value.IsNumber(values[i]) {
+			return value.NewNothing(), errors.New("All arguments must be numbers")
+		}
+		if i != 0 {
+			prevVal := values[i-1].(value.Number)
+			currentVal := values[i].(value.Number)
+
+			if prevVal.GetValue() <= currentVal.GetValue() {
+				return value.NewBoolean(false), nil
+			}
+		}
+	}
+
+	return value.NewBoolean(true), nil
+}
+
 var stdlibMap = map[string]func([]value.Value) (value.Value, error){
 	"+": add,
 	"-": substract,
 	"=": equal,
+	">": greaterThan,
 }
 
 func GetFunc(name string) (lib func([]value.Value) (value.Value, error), ok bool) {
--- stdlib/stdlib_test.go
+++ stdlib/stdlib_test.go
@@ -258,3 +258,163 @@ func TestEqualShouldBeFalseWithDifferentValues(t *testing.T) {
 		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
 	}
 }
+
+func TestGreaterThanShouldReturnErrorIfNotAllValuesNumbers(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expectedErrorMessage := "All arguments must be numbers"
+
+	_, err := equal([]value.Value{
+		value.NewBoolean(true),
+		value.NewNumber(1.0),
+	})
+
+	if err == nil {
+		t.Errorf("Should return an error")
+		return
+	}
+
+	if err.Error() != expectedErrorMessage {
+		t.Errorf("\nError message should be:\n%s\nWas:\n%s", expectedErrorMessage, err.Error())
+	}
+}
+
+func TestGreaterThanShouldReturnFalseWithNoArguments(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expected := value.NewBoolean(false)
+
+	result, err := equal([]value.Value{})
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
+
+func TestGreaterThanShouldReturnTrueWithOneArgument(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expected := value.NewBoolean(true)
+
+	result, err := equal([]value.Value{
+		value.NewBoolean(false),
+	})
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
+
+func TestGreaterThanAnArgGreaterThanAnOther(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expected := value.NewBoolean(true)
+
+	result, err := equal([]value.Value{
+		value.NewNumber(4),
+		value.NewNumber(2),
+	})
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
+
+func TestGreaterThanAnArgGreaterThanMultipleOthers(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expected := value.NewBoolean(true)
+
+	result, err := equal([]value.Value{
+		value.NewNumber(4),
+		value.NewNumber(2),
+		value.NewNumber(1),
+	})
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
+
+func TestGreaterThanAnArgNotGreaterThanMultipleOthers(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expected := value.NewBoolean(false)
+
+	result, err := equal([]value.Value{
+		value.NewNumber(2),
+		value.NewNumber(10),
+		value.NewNumber(1),
+	})
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
+
+func TestGreaterThanAnArgEqualToAnOther(t *testing.T) {
+	equal, ok := GetFunc(">")
+	if !ok {
+		t.Errorf("> function not found")
+		return
+	}
+
+	expected := value.NewBoolean(false)
+
+	result, err := equal([]value.Value{
+		value.NewNumber(10),
+		value.NewNumber(10),
+	})
+
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Not equal.\nExpected:\n%#v\nReceived:\n%#v", expected, result)
+	}
+}
